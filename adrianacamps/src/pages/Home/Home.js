8565a0c5ae5b931be60d48c4b8f3ec48b81919ad
Sebastian Brupbacher <sebastian.eliasb@gmail.com>
2023-01-12 13:09:46 +0100

    Home static design

--- adrianacamps/src/pages/Home/Home.js
+++ adrianacamps/src/pages/Home/Home.js
@@ -2,15 +2,25 @@ import React from "react";
 import "./style/home.scss";
 
 function Home() {
+  const handleNavTransform = () => {};
   return (
     <>
       <main className="home-main">
         <div className="carrusel">
           {" "}
-          <div className="adriana-name">
-            <div>ADRIANA</div>
-            <span>Interior & Lighting Studio</span>
-            <div>CAMPS</div>
+          <div className="main-container-big">
+            <div className="left-small"></div>
+            <div className="adriana-name">
+              <div>ADRIANA</div>
+              <span>Interior & Lighting Studio</span>
+              <div>CAMPS</div>
+            </div>
+            <div className="right-small">
+              <nav className="nav">
+                <div></div>
+                <div></div>
+              </nav>
+            </div>
           </div>
           <div className="copy-info">
             <span>Instagram — Pinterest — LinkedIn</span>
