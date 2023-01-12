83ffd1bfdf359a1681cec24dbfe90157bf4b080c
Sebastian Brupbacher <sebastian.eliasb@gmail.com>
2023-01-12 17:17:11 +0100

    resorting of scss

--- adrianacamps/src/layouts/RootLayout.js
+++ adrianacamps/src/layouts/RootLayout.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
-import Nav from "../Components/Nav";
 import "./rootlayout.scss";
 
 function RootLayout() {
   return (
     <div className="root-layout">
-      <Nav />
       <Outlet />
     </div>
   );
--- adrianacamps/src/pages/Home/Home.js
+++ adrianacamps/src/pages/Home/Home.js
@@ -2,33 +2,32 @@ import React from "react";
 import "./style/home.scss";
 
 function Home() {
-  const handleNavTransform = () => {};
   return (
     <>
       <main className="home-main">
-        <div className="carrusel">
-          {" "}
-          <div className="main-container-big">
-            <div className="left-small"></div>
-            <div className="adriana-name">
-              <div>ADRIANA</div>
-              <span>Interior & Lighting Studio</span>
-              <div>CAMPS</div>
-            </div>
-            <div className="right-small">
-              <nav className="nav">
-                <div></div>
-                <div></div>
-              </nav>
-            </div>
+        {" "}
+        <div className="main-container-big">
+          <div className="left-small"></div>
+          <div className="adriana-name">
+            <div>ADRIANA</div>
+            <span>Interior & Lighting Studio</span>
+            <div>CAMPS</div>
           </div>
-          {/* <div className="copy-info">
+          <div className="right-small">
+            <nav>
+              <div></div>
+              <div></div>
+            </nav>
+          </div>
+        </div>
+        {/* <div className="copy-info">
             <span>Instagram — Pinterest — LinkedIn</span>
             <span>Design by Sauras Garriga</span>
             <span>© ADRIANA CAMPS 2023 — All rights reserved.</span>
           </div> */}
-        </div>
-
+        {/* Future component */}
+        <div className="carrusel"></div>
+        {/*  */}
         <div className="backdrop"></div>
       </main>
     </>
