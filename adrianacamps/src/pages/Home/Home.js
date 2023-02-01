6b37f1eb4950a2ca08adc158424283e4c5122717
Sebastian Brupbacher <sebastian.eliasb@gmail.com>
2023-02-01 12:39:39 +0100

    created a layout component

--- adrianacamps/src/pages/Home/Home.js
+++ adrianacamps/src/pages/Home/Home.js
@@ -3,44 +3,30 @@ import NameBox from "../../Components/nameBox";
 import "./style/home.scss";
 import Nav from "../../Components/nav";
 import Carrusel from "../../Components/Carrusel";
+import NameLayout from "../../layouts/nameLayout";
 
 function Home() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleNav = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
     <>
       <Carrusel />
-
-      {!isOpen ? (
-        <>
-          <NameBox
-            color={"white"}
-            navColor={"white"}
-            text={<span>Interior & Lighting Studio</span>}
-            toggleNav={toggleNav}
-            navClass={"nav-open"}
-          />
-          <main className="home-main">
-            <footer>
-              <div>
-                <span>Instagram — Pinterest — LinkedIn </span>
-                <span>© Adriana Camps 2023 — All Rights reserved</span>
-              </div>
-              <div>
-                <span>Design by Sauras Garriga</span>
-              </div>
-            </footer>
-          </main>
-        </>
-      ) : (
-        <>
-          <Nav toggleNav={toggleNav} />
-        </>
-      )}
+      <NameLayout
+        color={"white"}
+        navColor={"white"}
+        text={<span>Interior & Lighting Studio</span>}
+        navClass={"nav-open"}
+      >
+        <main className="home-main">
+          <footer>
+            <div>
+              <span>Instagram — Pinterest — LinkedIn </span>
+              <span>© Adriana Camps 2023 — All Rights reserved</span>
+            </div>
+            <div>
+              <span>Design by Sauras Garriga</span>
+            </div>
+          </footer>
+        </main>
+      </NameLayout>
     </>
   );
 }
