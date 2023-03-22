73e88dcccb5ca95d7cec8d299ac5886dcba1573e
Sebastian Brupbacher <sebastian.eliasb@gmail.com>
2023-03-22 14:34:09 +0100

    main push aws

--- adrianacamps/src/pages/error-page/error-page.jsx
+++ adrianacamps/src/pages/error-page/error-page.jsx
@@ -1,11 +1,10 @@
 import { useRouteError } from "react-router-dom";
-import NameLayout from "../../layouts/nameLayout/NameLayout";
+
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <NameLayout>
     <div id="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
@@ -13,6 +12,5 @@ export default function ErrorPage() {
         <i>{error.statusText || error.message}</i>
       </p>
     </div>
-    </NameLayout>
   );
 }
--- adrianacamps/src/routes/root.jsx
+++ adrianacamps/src/routes/root.jsx
@@ -1,11 +1,10 @@
 import React from "react"
 import Home from "../pages/Home"
 import { Outlet } from "react-router-dom";
-import Comming from "../pages/CommigSoon/Coming";
 export default function Root() {
     return(
 <>
-<Comming/>
+<Home/>
 <Outlet/>
 </>
     )
