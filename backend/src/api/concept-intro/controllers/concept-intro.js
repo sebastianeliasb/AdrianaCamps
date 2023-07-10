aca23541962a200b690808046fd346aee582f874
Sebastian Brupbacher <sebastian.eliasb@gmail.com>
2023-07-10 18:53:46 +0200

    database creation all collection

---
+++ backend/src/api/concept-intro/controllers/concept-intro.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * concept-intro controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::concept-intro.concept-intro');
---
+++ backend/src/api/concept/controllers/concept.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * concept controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::concept.concept');
